9174fb88b9d41037eb1cc568b0c3bdd765dcae6e
Vian Tang <vian.tang@live.com>
2017-07-25 22:21:39 +0800

    change resource location

--- src/main/java/com/askprice/carprice/common/config/WebConfig.java
+++ src/main/java/com/askprice/carprice/common/config/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("/");
-        }
-}
+//@Configuration
+//@EnableWebMvc
+//@ComponentScan
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**").addResourceLocations("/");
+//        }
+//}
