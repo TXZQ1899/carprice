df8dc8db21dd47258de1fcbf1373dafbe2164189
Vian Tang <vian.tang@live.com>
2017-08-02 01:28:53 +0800

    fix email issue

--- src/main/java/com/askprice/carprice/common/util/excel/TemplateFileUtil.java
+++ src/main/java/com/askprice/carprice/common/util/excel/TemplateFileUtil.java
@@ -1,13 +1,14 @@
 package com.askprice.carprice.common.util.excel;
 
-import org.springframework.util.ResourceUtils;
-
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class TemplateFileUtil {
 
-    public static FileInputStream getTemplates(String tempName) throws FileNotFoundException {
-        return new FileInputStream(ResourceUtils.getFile("classpath:templates/"+tempName));
+    public static InputStream getTemplates(String templateName) throws FileNotFoundException {
+    	InputStream is = new FileInputStream(new File("/usr/local/output/templates/" + templateName));
+    	return is;
     }
 }
