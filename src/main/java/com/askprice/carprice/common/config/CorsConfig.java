3264956cf8c31d117725b6f7c8af87309c910c74
Vian Tang <vian.tang@live.com>
2017-08-05 17:16:21 +0800

    remove useless import

--- src/main/java/com/askprice/carprice/common/config/CorsConfig.java
+++ src/main/java/com/askprice/carprice/common/config/CorsConfig.java
@@ -1,7 +1,5 @@
 package com.askprice.carprice.common.config;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
